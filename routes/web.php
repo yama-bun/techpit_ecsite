4a197e88f5cc68019d172be81d0534256ff9f7d7
yama-bun <baseball8296@gmail.com>
2021-03-17 21:30:00 +0900

    test

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', 'ItemController@index');
+
+Route::get('/cartitem', 'CartItemController@index');
+
+Route::post('/cartitem', 'CartItemController@store');
+
+Route::delete('/cartitem/{cartItem}', 'CartItemController@destroy');
+
+Route::put('/cartitem/{cartItem}', 'CartItemController@update');
+
+Route::get('/item/{item}', 'ItemController@show');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
